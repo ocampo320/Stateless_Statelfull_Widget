c88e85b9c9eec10419fed64808c4ce548f97ebf3
deivi ocampo <deibi.ocampo@sofka.com.co>
2020-08-21 12:10:06 -0500

    se agrega comentarios

--- lib/main.dart
+++ lib/main.dart
@@ -5,65 +5,70 @@ void main() {
 }
 
 /*
-En este  codigo se da el ejemplo con StatefulWidget, con estado, el widget es mutable
+Widget con  estado, se  construye las veces que se requiera
  */
 
+//
 //class Prueba extends StatefulWidget {
 //  @override
 //  _PruebaState createState() => _PruebaState();
 //}
+//
 
-//int valor = 0;
-
+//
 //class _PruebaState extends State<Prueba> {
+//int contador = 0;
 //  @override
 //  Widget build(BuildContext context) {
 //    return MaterialApp(
 //      home: SafeArea(
 //        child: Scaffold(
-//          appBar: AppBar(
-//            title: Text("Sin estado"),
-//          ),
-//          body: (Container(
-//            child: Text(valor.toString()),
-//          )),
-//          floatingActionButton: FloatingActionButton(
-//            child: Icon(Icons.add),
-//            onPressed: () {
-//              setState(() {});
-//              print(valor++);
-//            },
-//          ),
-//        ),
+//            appBar: AppBar(
+//              title: Text("Sin estado"),
+//            ),
+//            body: Container(
+//              child: Column(
+//                children: <Widget>[Text(contador.toString())],
+//              ),
+//            ),
+//            floatingActionButton: FloatingActionButton(
+//              child: Icon(Icons.add),
+//              onPressed: () {
+//                setState(() {});
+//                print(contador++);
+//              },
+//            )),
 //      ),
 //    );
 //  }
 //}
 
 /*
-En este  codigo se da el ejmplo con StatelessWidget, sin estado, el widget es inmutable
+Widget sin estado, solo se  construye una vez
  */
 
 class Prueba extends StatelessWidget {
+  int contador = 0;
   @override
   Widget build(BuildContext context) {
-    int valor = 0;
     return MaterialApp(
       home: SafeArea(
         child: Scaffold(
-          appBar: AppBar(
-            title: Text("Sin estado"),
-          ),
-          body: (Container(
-            child: Text(valor.toString()),
-          )),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              print(valor++);
-            },
-            child: Icon(Icons.add),
-          ),
-        ),
+            appBar: AppBar(
+              title: Text("Sin estado"),
+            ),
+            body: Container(
+              child: Column(
+                children: <Widget>[Text(contador.toString())],
+              ),
+            ),
+            floatingActionButton: FloatingActionButton(
+              child: Icon(Icons.add),
+              onPressed: () {
+                contador++;
+                print(contador);
+              },
+            )),
       ),
     );
   }
